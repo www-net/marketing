f6339c0ee59b58eeb0b33bf3eed9b62f4bc8eaf1
www-net <78052177+www-net@users.noreply.github.com>
2022-04-24 14:31:46 +0300

    added blog

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function(){
     var mixer = mixitup('.portfolio__content');
+
+    $('.slider-blog__inner').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>',
+    });
+
 });
