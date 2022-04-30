48b9c8e8eb97a2d1f01259b8cd6c600b8e3e09cc
www-net <78052177+www-net@users.noreply.github.com>
2022-04-30 19:46:04 +0300

    added smooth scroll and sticky header

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $(function () {
 
+    $(".menu a, .go-top").on("click", function (e) {
+		//отменяем стандартную обработку нажатия по ссылке
+		e.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
     $('.slider-blog__inner').slick({
         dots: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
@@ -14,7 +28,7 @@ $(function () {
         ]
     });
 
-    $('.menu__btn').on('click', function () {
+    $('.menu__btn, .menu a').on('click', function () {
         $('.header__top-inner').toggleClass('header__top-inner--active');
     });
 
